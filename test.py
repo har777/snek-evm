6fa1f383246af9f96a65ae4cb4bc0c5dcbb8a911
Harikrishnan Shaji <hihari777@gmail.com>
2023-11-19 00:13:08 +0530

    Implement STOP and PUSH instructions + simple tests

--- test.py
+++ test.py
@@ -4,4 +4,50 @@ from vm import Contract
 
 
 class ContractTestCase(unittest.TestCase):
-    pass
+    def test_stop(self):
+        # https://www.evm.codes/playground?fork=shanghai&unit=Wei&codeType=Bytecode&code='600a00600a'_
+        # PUSH1 0x0a
+        # STOP
+        # PUSH1 0x0a
+        contract = Contract(bytecode="600a00600a")
+        contract.execute()
+        self.assertEqual(contract.stack, ["a"])
+
+    def test_push0(self):
+        # https://www.evm.codes/playground?fork=shanghai&unit=Wei&codeType=Bytecode&code='5f5f'_
+        # PUSH0
+        # PUSH0
+        contract = Contract(bytecode="5f5f")
+        contract.execute()
+        self.assertEqual(contract.stack, ["0", "0"])
+
+    def test_push1(self):
+        # https://www.evm.codes/playground?fork=shanghai&unit=Wei&codeType=Bytecode&code='60ff6000'_
+        # PUSH1 0xff
+        # PUSH1 0x00
+        contract = Contract(bytecode="60ff6000")
+        contract.execute()
+        self.assertEqual(contract.stack, ["ff", "0"])
+
+    def test_push31(self):
+        # https://www.evm.codes/playground?fork=shanghai&unit=Wei&codeType=Bytecode&code='7eyyyyyw7exxxxxv'~wwwzvvvy~~xzzwffv00%01vwxyz~_
+        # PUSH31 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+        # PUSH31 0x00000000000000000000000000000000000000000000000000000000000000
+        contract = Contract(bytecode="7effffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff7e00000000000000000000000000000000000000000000000000000000000000")
+        contract.execute()
+        self.assertEqual(contract.stack, ["ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff", "0"])
+
+        # https://www.evm.codes/playground?fork=shanghai&unit=Wei&codeType=Bytecode&code='7ezzzzz7e7e0a'~ffffffz~~%01z~_
+        # PUSH31 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff7e
+        # PUSH31 0x0a
+        contract = Contract(bytecode="7effffffffffffffffffffffffffffffffffffffffffffffffffffffffffff7e7e0a")
+        contract.execute()
+        self.assertEqual(contract.stack, ["ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff7e", "a"])
+
+    def test_push32(self):
+        # https://www.evm.codes/playground?fork=shanghai&unit=Wei&codeType=Bytecode&code='7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff7f0000000000000000000000000000000000000000000000000000000000000000'_
+        # PUSH32 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF
+        # PUSH32 0x0000000000000000000000000000000000000000000000000000000000000000
+        contract = Contract(bytecode="7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff7f0000000000000000000000000000000000000000000000000000000000000000")
+        contract.execute()
+        self.assertEqual(contract.stack, ["ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff", "0"])
--- vm.py
+++ vm.py
@@ -34,6 +34,39 @@ class Contract:
 
         opcode = self.parsed_bytecode[self.program_counter]
 
+        # STOP
+        if opcode == "00":
+            self.program_counter += 1
+            self.stopped = True
+            return
+
+        # PUSH0
+        elif opcode == "5f":
+            self.program_counter += 1
+            self.stack.append("0")
+            return
+
+        # PUSH1 to PUSH32
+        elif opcode in {"60", "61", "62", "63", "64", "65", "66", "67",
+                        "68", "69", "6a", "6b", "6c", "6d", "6e", "6f",
+                        "70", "71", "72", "73", "74", "75", "76", "77",
+                        "78", "79", "7a", "7b", "7c", "7d", "7e", "7f"}:
+            self.program_counter += 1
+            num_of_bytes = int(opcode, 16) - 95
+
+            bytes_list = deque([])
+            for _ in range(num_of_bytes):
+                try:
+                    val = self.parsed_bytecode[self.program_counter]
+                    bytes_list.append(val)
+                    self.program_counter += 1
+                except IndexError:
+                    val = "00"
+                    bytes_list.appendleft(val)
+            bytes_num = hex(int("".join(bytes_list), 16))[2:]
+            self.stack.append(bytes_num)
+            return
+
         raise Exception(f"OPCODE {opcode} not implemented")
 
     def execute(self):
